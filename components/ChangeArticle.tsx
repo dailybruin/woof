4e41b4fde118fbf1b328dd0aa05423502b72964e
Tyler Tran <tylerduytran@gmail.com>
2024-04-23 16:59:06 -0700

    Styling for the background color and the ChangeArticle component for font and color

--- components/ChangeArticle.tsx
+++ components/ChangeArticle.tsx
@@ -1,13 +1,14 @@
 
 import Link from 'next/link';
+// import '../css/body.css';
 
 export default function ChangeArticle() {
     return (
         
-        <div className="lower-header">
-            <div>Need to edit or create an article?</div>
-            <Link href="/">Home</Link>
-            <Link href="/new">Add Article</Link>
+        <div className="edit-article">
+            <div className="edit-heading">Need to edit or create an article?</div>
+            {/* <Link href="/">Home</Link> */}
+            <Link href="/new" className="edit-link">Click Here</Link>
       </div>
     );
 }
