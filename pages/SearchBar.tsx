8aa512193ffd5c3d2eae6b6191fca71602094445
Tyler Tran <tylerduytran@gmail.com>
2024-04-23 03:19:14 -0700

    Partially styled and formatted Search function

--- pages/SearchBar.tsx
+++ pages/SearchBar.tsx
@@ -5,6 +5,8 @@ import Article, { Articles } from '../models/article';
 import { GetServerSideProps } from 'next';
 import { GetStaticProps } from 'next';
 import { useEffect, useState } from 'react';
+// import bootstrap from 'bootstrap';
+
 
 export type Props = {
   articles: Articles[];
@@ -33,13 +35,20 @@ const SearchBar = ({ articles }: Props) => {
 
 
     return (
-        <div className="text-3xl font-bold leading-10 text-left">
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={e => setSearchTerm(e.target.value)}
-                placeholder="Search articles..."
-            />
+        <div className="w-1/3">
+            <div className="full-search-bar">
+                <input
+                    className="search-bar"
+                    type="text"
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
+                    placeholder="Search articles..."
+                /> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg> */}
+            </div>
             {/* {filteredArticles.map((article) => (
                 <div key={article._id}>
                 <Link href={{
@@ -51,7 +60,7 @@ const SearchBar = ({ articles }: Props) => {
                 </div>
             ))} */}
             {searchTerm && filteredArticles.map((article) => (
-                <div key={article._id}>
+                <div key={article._id} className="search-results">
                     <Link href={{
                     pathname: '/[id]',
                     query: { id: article._id },
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import '../css/form.css'; // todo: move to globals
 import '../styles/globals.css';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import SearchBar from './SearchBar';
+import './_app.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // console.log(session, 'session')
@@ -17,13 +19,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <title>Woof</title>
       </Head>
       <div className="top-bar">
-        <div className="nav">
+        <div className="header">
+          <div className="woof-img-container">
+            <Image id="title" src="/Woof-Logo.png" width={150} height={150} alt="Woof logo"></Image>
+          </div>
           <SearchBar articles={pageProps.articles}/>
+        </div>
+        <div className="nav">
           <Link href="/">Home</Link>
           <Link href="/new">Add Article</Link>
         </div>
-
-        <img id="title" src="/favicon.ico" alt="Woof logo"></img>
       </div>
       <div className="wrapper grid">
         <Component {...pageProps} />
