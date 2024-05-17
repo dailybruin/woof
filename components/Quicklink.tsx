fe538bfb5676188a411a4d5eed6f3b8f9f4eeec5
Bach Ngo <ngo.b48@gmail.com>
2024-05-17 02:05:53 -0700

    fixed quicklink box font and made responsive

--- components/Quicklink.tsx
+++ components/Quicklink.tsx
@@ -3,32 +3,38 @@ import { Props } from '../pages/index';
 
 
 const Quicklink = ({ articles }: Props) => {
-    // console.log(articles)
+    // console.log(articles.map(a => a.title))
     return (
         <>
             <div 
-            className='rounded-xl border-black border-t-4 border-l-4 border-b-8 border-r-8 w-96'
+            className='rounded-2xl border-black border-t-[0.5vmin] border-l-[0.5vmin] border-b-[0.8vmin] border-r-[0.8vmin] w-[40vmin] absolute right-[7vmin] top-[18vmin]'
             >
                 <div
-                className='border-black border-b-4 bg-purple-400 h-20 w-full rounded-t-md items-center pl-8 flex'
+                className='border-black border-b-[0.5vmin] bg-purple-400 h-[8vmin] w-full rounded-t-lg items-center pl-[3vmin] flex'
                 >
                     <p
-                    className='font-black text-center text-4xl justify-center text-white'
+                    className='font-semibold text-center text-[4vmin] justify-center text-white bg-transparent quick-links pt-[1vmin]'
                     >
                         Quick Links
                     </p>
 
                 </div>
                 <div
-                className='py-6 px-10 '
+                className='py-[1.2vmin] px-[3.7vmin] rounded-b-lg'
                 >
-                    {articles.filter(a => a.quick_link).map(a => (
+                    {(articles && articles?.length > 0) ? (articles.filter(a => a.quick_link).map(a => (
                         <p
-                        className='text-blue py-1 text-lg font-bold'
+                        className='text-black py-[0.5vmin] text-[1.8vmin] font-bold bg-transparent quick-links'
                         >
                             <a href={a.image_url}>{a.title}</a>
                         </p>
-                    ))}
+                    ))) : (
+                        <p
+                        className='py-[0.5vmin] text-[1.8vmin] font-bold text-black bg-transparent quick-links'
+                        > 
+                            No Quick Links Available 
+                        </p>
+                    )}
                 </div>
             </div>
         </>
