55fa8c63251f68163b221b65aef0b06d6c67e142
Matthew Yu <56417255+matthewyu01@users.noreply.github.com>
2024-06-11 02:06:04 -0700

    Color Navbar bottom border based off section/route (#26)
    * Add all-color for simpler code
    
    * Set navbar bottom border color to current section color
    
    * Add color for /new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const ACCENT_PURPLE = '#C077CC';
 
-const sectionColors = {
-  'accent-purple': ' #C077CC',
+const dynamicRouteColors = {
+  'all-color': ACCENT_PURPLE,
   'news-color': '#0A97D0',
   'opinion-color': '#6DA81E',
   'arts-color': '#E98C1B',
   'sports-color': '#3744B5',
   'misc-color': '#1B8E50',
   'troubleshooting-color': '#D249BC',
+  'new-color': ACCENT_PURPLE,
+};
+const otherDynamicColors = {
+  'accent-purple': ACCENT_PURPLE,
+};
+const dynamicColors = {
+  ...dynamicRouteColors,
+  ...otherDynamicColors,
 };
 const customColors = {
   white: '#FCFCFC',
 };
-const colors = { ...sectionColors, ...customColors };
+const colors = { ...dynamicColors, ...customColors };
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  safelist: Object.keys(sectionColors).flatMap((color) => [
+  safelist: Object.keys(dynamicColors).flatMap((color) => [
     `bg-${color}`,
     `group-hover:text-${color}`,
     `text-${color}`,
+    `border-${color}`,
   ]),
   theme: {
     extend: {
