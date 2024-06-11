a201da3eab46edb6d31a5477782cfaf05d5f35a0
Matthew Yu <56417255+matthewyu01@users.noreply.github.com>
2024-06-10 20:42:57 -0700

    Fix Troubleshooting color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ const colors = {
   'arts-color': '#E98C1B',
   'sports-color': '#3744B5',
   'misc-color': '#1B8E50',
-  'troubleshooting-color': '#D249B',
+  'troubleshooting-color': '#D249BC',
 };
 module.exports = {
   content: [
