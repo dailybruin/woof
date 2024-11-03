b1d3d5ee68050e7f0e2be6baf5cc46d3f19ea5fd
Tyler Tran <tylertran1218@gmail.com>
2024-11-03 10:34:03 -0800

    Woof-3 Fix input (#43)
    * Woof-3 : documentation additions
    
    * Woof-3 : final updates to documentation
    
    * Woof-3 : Restructured the design
    
    * Woof-3 : Refactored entire folder layout structure
    
    * Woof-3 : fixed input bug

--- components/body/Quicklink.tsx
+++ components/body/Quicklink.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Articles } from '../models/article';
+import { Articles } from '../../models/article';
 type Props = {
   articles: Articles[];
   color?: string;
