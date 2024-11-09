a2b503b2b840b1cd53833261dad8dba730d04f68
laurenrm3 <115776428+laurenrm3@users.noreply.github.com>
2024-11-08 16:12:16 -0800

    Woof-8 : finished styling for article body card with raw text

--- components/body/Quicklink.tsx
+++ components/body/Quicklink.tsx
@@ -10,37 +10,39 @@ const Quicklink = ({ articles, color = 'accent-purple' }: Props) => {
 
   return (
     <>
-      <div className="rounded-2xl border-black border-t-[0.5vmin] border-l-[0.5vmin] border-b-[0.8vmin] border-r-[0.8vmin] w-[40vmin] ">
-        <div
-          className={`border-black border-b-[0.5vmin] bg-${color} h-[8vmin] w-full rounded-t-lg items-center pl-[3vmin] flex`}
-        >
-          <p className="font-semibold text-center text-[4vmin] justify-center text-white bg-transparent quick-links pt-[1vmin]">
-            Quick Links
-          </p>
-        </div>
-        <div className="py-[1.2vmin] px-[3.7vmin] rounded-b-lg">
-          {quickLinks.length > 0 ? (
-            articles
-              .filter((article) => article.quick_link)
-              .map((article) => (
-                <div key={article._id}>
-                  <p className="text-black py-[0.5vmin] text-[1.8vmin] font-bold bg-transparent quick-links">
-                    <Link
-                      href={{
-                        pathname: '/[id]',
-                        query: { id: article._id },
-                      }}
-                    >
-                      {article.title}
-                    </Link>
-                  </p>
-                </div>
-              ))
-          ) : (
-            <p className="py-[0.5vmin] text-[1.8vmin] font-bold text-black bg-transparent quick-links">
-              No Quick Links Available
+      <div className={`flex min-h-screen flex-col`}>
+        <div className="bg-white m-8 w-[50vmin] rounded-2xl border-black border-t-[0.5vmin] border-l-[0.5vmin] border-b-[0.8vmin] border-r-[0.8vmin]  ">
+          <div
+            className={`border-black border-b-[0.5vmin] bg-${color} h-[8vmin] w-full rounded-t-lg items-center pl-[3vmin] flex`}
+          >
+            <p className="font-semibold text-center text-[4vmin] justify-center text-white bg-transparent quick-links pt-[1vmin]">
+              Quick Links
             </p>
-          )}
+          </div>
+          <div className="py-[1.2vmin] px-[3.7vmin] rounded-b-lg">
+            {quickLinks.length > 0 ? (
+              articles
+                .filter((article) => article.quick_link)
+                .map((article) => (
+                  <div key={article._id}>
+                    <p className="text-black py-[0.5vmin] text-[1.8vmin] font-bold bg-transparent quick-links">
+                      <Link
+                        href={{
+                          pathname: '/[id]',
+                          query: { id: article._id },
+                        }}
+                      >
+                        {article.title}
+                      </Link>
+                    </p>
+                  </div>
+                ))
+            ) : (
+              <p className="py-[0.5vmin] text-[1.8vmin] font-bold text-black bg-transparent quick-links">
+                No Quick Links Available
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </>
