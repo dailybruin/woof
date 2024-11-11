4f899a580522aa04c4fd458077ddcb6fd4c60724
angoway <ngoamandauyen@gmail.com>
2024-11-11 01:05:51 -0800

    Woof-13 : Changed font sizing and placed add/edit article component to the right side

--- components/edit/ChangeArticle.tsx
+++ components/edit/ChangeArticle.tsx
@@ -2,12 +2,15 @@ import Link from 'next/link';
 
 export default function ChangeArticle() {
   return (
-    <div className="edit-article">
-      <div className="edit-heading">Need to edit or create an article?</div>
-      {/* <Link href="/">Home</Link> */}
-      <Link href="/new" className="edit-link">
-        Click Here
-      </Link>
+    <div className="change-article">
+      <div className="edit-article">
+        <div className="edit-heading">Need to edit or create an article?</div>
+        {/* <Link href="/">Home</Link> */}
+        <Link href="/new" className="edit-link">
+          Click Here
+        </Link>
+      </div>
     </div>
+    
   );
 }
