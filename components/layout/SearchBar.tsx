b47c88ef61ce2a0743337fe745d8ffacc95590f6
laurenrm3 <lrmirhan@gmail.com>
2024-11-16 21:31:13 -0800

    Woof-14 : added magnifying glass icon to search bar (#56)
    Co-authored-by: Tyler Tran <tylertran1218@gmail.com>

--- components/layout/SearchBar.tsx
+++ components/layout/SearchBar.tsx
@@ -28,23 +28,36 @@ const SearchBar = ({articles}: Props) => {
   return (
     <div className="w-1/3 search-bar-main">
       <div className="full-search-bar">
-        <input
-          className="search-bar"
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search articles..."
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-search"
-          viewBox="0 0 16 16"
-        >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
+        <div style={{ position: 'relative', width: '100%' }}>
+          <input
+            className="search-bar"
+            type="text"
+            style={{
+              width: '100%',
+            }}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search articles..."
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-search"
+            viewBox="0 0 16 16"
+            style={{
+              width: '20px',
+              height: '20px',
+              position: 'absolute',
+              top: '50%',
+              right: '8px',
+              transform: 'translateY(-50%)',
+            }}
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </div>
       </div>
     </div>
   );
