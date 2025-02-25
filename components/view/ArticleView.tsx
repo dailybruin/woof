0a0ab15fb332de089d72dc901c2e2c5890c7d525
Tyler Tran <tylertran1218@gmail.com>
2025-02-24 19:22:17 -0800

    Woof-69: Styled the articles section (#72)

--- components/view/ArticleView.tsx
+++ components/view/ArticleView.tsx
@@ -1,5 +1,8 @@
 // components/view/ArticleView.tsx
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import NavBar from '../layout/NavBar';
+import { useRouter } from 'next/router';
 
 interface ArticleViewProps {
   article: {
@@ -16,28 +19,41 @@ interface ArticleViewProps {
 }
 
 const ArticleView: React.FC<ArticleViewProps> = ({ article }) => {
+  const location = useRouter();
+  const pathname = location.pathname;
+  // const articleId = pathname.substring(pathname.lastIndexOf('/') + 1);
+  
   return (
-    <div className="article-view">
-      <h1>{article.title}</h1>
-      {article.image_url && <img src={article.image_url} alt={article.title} />}
-      <p>{article.content}</p>
-      <p>
-        <strong>Created:</strong> {new Date(article.created_date).toLocaleString()}
-      </p>
-      <p>
-        <strong>Last Updated:</strong> {new Date(article.updated_date).toLocaleString()}
-      </p>
-      {article.sections && (
-        <div>
-          <strong>Sections:</strong>
-          <ul>
-            {article.sections.map((section) => (
-              <li key={section}>{section}</li>
-            ))}
-          </ul>
+    <div>
+      <NavBar pathname={pathname}/>
+      <div className="article-view" style={{ margin: '2em', border: '5px solid #000000', borderRadius: '1em', fontFamily: 'Rockwell, sans-serif' }}>
+      <div style={{ backgroundColor: '#C077CC', padding: '1em', borderRadius: '0.45em 0.45em 0em 0em', fontSize: '24px', borderBottom: '4px solid #000000'}}>
+        <h1 style={{ color: '#FFFEE4' }}>{article.title}</h1>
+      </div> 
+      <div style={{ backgroundColor: '#FFFFFF', borderRadius: '0em 0em 0.60em 0.60em', padding: '1em' }}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {article.image_url && <img src={article.image_url} alt={article.title} style={{ maxWidth: '100%', maxHeight: '300px', width: 'auto', height: 'auto' }} />}
         </div>
-      )}
-      {article.quick_link && <p>This is a Quick Link article.</p>}
+        <p className={'prose'}> <ReactMarkdown>{article.content}</ReactMarkdown></p>
+        <p>
+          <strong>Created:</strong> {new Date(article.created_date).toLocaleString()}
+        </p>
+        <p>
+          <strong>Last Updated:</strong> {new Date(article.updated_date).toLocaleString()}
+        </p>
+        {article.sections && (
+          <div>
+            <strong>Sections:</strong>
+            <ol>
+              {article.sections.map((section) => (
+                <li key={section}>{section}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+        {article.quick_link && <p>This is a Quick Link article.</p>}
+      </div>
+    </div>
     </div>
   );
 };
